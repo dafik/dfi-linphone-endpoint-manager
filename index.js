34f2c0f3b5dbc1134b110fdbc01714fe7482323c
Zbyszek Wieczorek <zbyszek.wieczorek@gmail.com>
2017-07-28 13:17:56 +0200

    ts fixes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const EndpointManager = require("./src/endpointManager");
+const endpointManager_1 = require("./src/endpointManager");
 let _instance;
+var endpointManager_2 = require("./src/endpointManager");
+exports.EndpointManager = endpointManager_2.EndpointManager;
 function getInstance(server) {
     if (typeof _instance === "undefined") {
-        _instance = new EndpointManager(server);
+        _instance = new endpointManager_1.default(server);
     }
     return _instance;
 }
-exports.getInstance = getInstance;
+exports.default = getInstance;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import EndpointManager = require("./src/endpointManager");
+import EndpointManager from "./src/endpointManager";
+
 let _instance;
 
-export function getInstance(server): EndpointManager {
+export {EndpointManager} from "./src/endpointManager";
+export default function getInstance(server): EndpointManager {
     if (typeof _instance === "undefined") {
         _instance = new EndpointManager(server);
     }
